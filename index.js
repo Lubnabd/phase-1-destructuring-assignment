aa8139c1829dac6346a2952abf51ac307f76386e
Lubnabd <lubna.abdelkhaleq@gmail.com>
2024-03-10 00:30:52 +0300

    completed

--- index.js
+++ index.js
@@ -70,3 +70,4 @@ const [[b, animal1], [k, animal2], [p, animal3]] = animalsByColor;
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+console.log("lubna");
